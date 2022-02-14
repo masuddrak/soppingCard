23aa9218b3da6330b958d53486f77d43d7b8e76f
masudrana <masudranaacb@gmail.com>
2022-02-14 13:14:13 +0600

    total

--- app.js
+++ app.js
@@ -10,7 +10,25 @@ function incrasToDecres(id,priceID,price,ranges){
     nubers.value=previusNumber;
     const casingDefoltPrice=document.getElementById(priceID);
     casingDefoltPrice.innerText=previusNumber*price;
+    calculatorSubtotal()
 }
+// 
+function getInputValue(productInput){
+    const input=document.getElementById(productInput);
+    const inputValu=parseInt(input.value);
+    return inputValu;
+}
+function calculatorSubtotal(){
+    const TotalPhonePrice=getInputValue('phoneInput')*1219;
+    const TotalCasingPrice=getInputValue('clickNumber')*59;
+    const totalSub=TotalPhonePrice+TotalCasingPrice;
+    const taxs=totalSub/10;
+    const total=totalSub+taxs;
+    document.getElementById('subTotal').innerText=totalSub;
+    document.getElementById('taxTotal').innerText=taxs;
+    document.getElementById('totals').innerText=total;
+}
+// phone handelart
 document.getElementById('phoneBtnPlus').addEventListener('click',function(){
     incrasToDecres('phoneInput','phonePrice',1219,true)
 })
